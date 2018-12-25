144681d6f457a573132f56ab8a97b6c9db1a92cd
Ion D. Filho <iondrimba@gmail.com>
2018-12-25 21:40:31 -0200

    playing audios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
+/* eslint-disable no-undef */
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      tracks: [],
+      player: {},
+    }
+  }
+
+  componentDidMount() {
+    fetch(`http://api.soundcloud.com/users/${process.env.REACT_APP_SOUNDCLOUD_USER_ID}/playlists?client_id=${process.env.REACT_APP_SOUNDCLOUD_APP_CLIENT_ID}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        this.setState(() => ({
+          tracks: [...result[0].tracks]
+        }));
+
+        this.setupAudio();
+      });
+  }
+
+  setupAudio() {
+    this.audioElement = document.getElementById('audio');
+    this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    this.analyser = this.audioCtx.createAnalyser();
+
+    this.source = this.audioCtx.createMediaElementSource(this.audioElement);
+    this.source.connect(this.analyser);
+    this.source.connect(this.audioCtx.destination);
+
+    this.bufferLength = this.analyser.frequencyBinCount;
+
+    this.frequencyData = new Uint8Array(this.bufferLength);
+    this.audioElement.volume = .5;
+  }
+
+  onPlayClick = (evt) => {
+    const id = evt.currentTarget.attributes['track-id'].value;
+
+    this.audioElement.src = this.state.tracks[id].stream_url;
+
+    this.audioCtx.resume();
+    this.audioElement.play();
+  }
+
+  onPauseClick = (evt) => {
+    this.audioElement.pause();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ul>
+          {
+            this.state.tracks.map((track) => {
+              return <li key={track.id}>
+                <h2>{track.title}</h2>
+                <span>{track.user.username}</span>
+                <img src={track.artwork_url} alt={`album artwork from track ${track.title}`} />
+                <button onClick={this.onPlayClick} track-id={track.id}>play</button>
+                <button onClick={this.onPauseClick} track-id={track.id}>pause</button>
+              </li>
+            })
+          }
+        </ul>
       </div>
     );
   }

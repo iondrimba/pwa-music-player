506a4613a4180ec17f665a6c48f04a49765f3da3
Ion D. Filho <iondrimba@gmail.com>
2018-12-25 22:28:01 -0200

    add progress bar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@
 import React, { Component } from 'react';
 import './App.scss';
 
+const percent = (current, total) => {
+  return (current / total) * 100
+}
+
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
       tracks: [],
-      player: {},
+      audio: {},
+      progress: {}
     }
   }
 
@@ -19,7 +24,8 @@ class App extends Component {
       })
       .then((result) => {
         this.setState(() => ({
-          tracks: [...result[0].tracks]
+          tracks: [...result[0].tracks],
+          id: 0,
         }));
 
         this.setupAudio();
@@ -39,15 +45,37 @@ class App extends Component {
 
     this.frequencyData = new Uint8Array(this.bufferLength);
     this.audioElement.volume = .5;
+
+    this.audioElement.addEventListener('loadeddata', (evt) => {
+      console.log(evt.target.duration);
+    })
+
+    this.audioElement.addEventListener('timeupdate', (evt) => {
+      const scale = percent(evt.target.currentTime, evt.target.duration)/100;
+
+
+      const item = document.querySelector(`[item-id="${this.state.id}"]`);
+      item.querySelector('.progress-bar');
+
+      console.log(scale);
+
+      item.querySelector('.progress-bar').style = `transform: scaleX(${scale})`;
+
+    })
   }
 
   onPlayClick = (evt) => {
     const id = evt.currentTarget.attributes['track-id'].value;
+    const track = this.state.tracks.filter((track) => Number(id) === track.id)[0];
 
-    this.audioElement.src = this.state.tracks[id].stream_url;
+    console.log(id, track, track.stream_url);
+
+    this.audioElement.src = `${track.stream_url}?client_id=${process.env.REACT_APP_SOUNDCLOUD_APP_CLIENT_ID}`;
 
     this.audioCtx.resume();
     this.audioElement.play();
+
+    this.setState({ id });
   }
 
   onPauseClick = (evt) => {
@@ -60,12 +88,13 @@ class App extends Component {
         <ul>
           {
             this.state.tracks.map((track) => {
-              return <li key={track.id}>
+              return <li key={track.id} item-id={track.id}>
                 <h2>{track.title}</h2>
                 <span>{track.user.username}</span>
                 <img src={track.artwork_url} alt={`album artwork from track ${track.title}`} />
                 <button onClick={this.onPlayClick} track-id={track.id}>play</button>
                 <button onClick={this.onPauseClick} track-id={track.id}>pause</button>
+                <div className="progress-bar"></div>
               </li>
             })
           }

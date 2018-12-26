5e5f2c059b065794b1894790336357746a54822a
Ion D. Filho <iondrimba@gmail.com>
2018-12-26 21:09:40 -0200

    add home styles

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { ReactComponent as AudioWave } from '../../icons/audio-wave.svg';
+import { ReactComponent as CD } from '../../icons/cd.svg';
+import { Link } from 'react-router-dom';
+import styles from './styles.scss';
 
 class Home extends Component {
   render() {
     return (
-      <div className="home">
-        <span>Home</span>
+      <div className="home page">
+        <CD className="cd" alt="CD icon." width="100"></CD>
+        <AudioWave className="audiowave" alt="CD icon." width="60"></AudioWave>
+        <Link
+          className="button"
+          to={{
+            pathname: "/list",
+            state: { fromHome: true }
+          }}
+        >Start listening!</Link>
       </div>
     );
   }

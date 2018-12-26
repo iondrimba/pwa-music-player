c269011c9cc7fc249076ed075cc0934bbe593b00
Ion D. Filho <iondrimba@gmail.com>
2018-12-26 21:10:05 -0200

    add common styles

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import Home from '../pages/Home/Home';
+import List from '../pages/List/List';
 import styles from './style.scss';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="shell">
-          <Router>
+        <Router>
+          <div className="shell">
             <Route path="/" exact component={Home} />
-          </Router>
-        </div>
-      </div>
+            <Route path="/List" exact component={List} />
+          </div>
+        </Router>
+      </div >
     );
   }
 }

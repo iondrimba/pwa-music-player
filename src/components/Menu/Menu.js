9eb70a0b73ebe0411cf4abaca0d0872e3830061e
Ion D. Filho <iondrimba@gmail.com>
2018-12-27 21:23:57 -0200

    add menu state

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,12 +4,27 @@ import { ReactComponent as BackButton } from '../../icons/left-arrow.svg';
 import styles from './styles.scss';
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onBackClick = this.onBackClick.bind(this);
+    this.onAboutClick = this.onAboutClick.bind(this);
+  }
+
+  onBackClick() {
+    this.props.onBackClick();
+  }
+
+  onAboutClick() {
+    this.props.onAboutClick();
+  }
+
   render() {
     return (
-      <nav className="menu">
-        <a href="/"><BackButton width={30}/></a>
-        <h1>Queue</h1>
-        <a href="/about"><HelpButton  width={30}/></a>
+      <nav className={`menu menu--${this.props.activeView}`}>
+        <BackButton width={30} onClick={this.onBackClick} />
+        <h1 className="menu__title">{this.props.activeView === 'list' ? 'Queue' : 'Now playing'}</h1>
+        <HelpButton className="help" onClick={this.onAboutClick} width={30} />
       </nav>
     );
   }

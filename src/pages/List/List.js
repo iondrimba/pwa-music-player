4b4de0e7b9ba3185c7f4a39203d1f3f4a372b501
Ion D. Filho <iondrimba@gmail.com>
2018-12-28 18:27:41 -0200

    remove unused code

--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -3,19 +3,6 @@ import React, { Component, Fragment } from 'react';
 import ProgressBar from '../../components/ProgressBar/ProgressBar';
 import styles from './styles.scss';
 
-const convertSecondsToMMss = (totalSeconds) => {
-  const sec_num = parseInt(totalSeconds, 10);
-  let hours = Math.floor(sec_num / 3600);
-  let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-  let seconds = sec_num - (hours * 3600) - (minutes * 60);
-
-  if (hours < 10) { hours = "0" + hours; }
-  if (minutes < 10) { minutes = "0" + minutes; }
-  if (seconds < 10) { seconds = "0" + seconds; }
-
-  return `${minutes}:${seconds}`;
-}
-
 class List extends Component {
   constructor(props) {
     super(props);
@@ -37,7 +24,7 @@ class List extends Component {
             this.props.tracks.map((track) => {
               return <li className="row" key={track.id} item-id={track.id} onClick={this.onClick}>
                 <div className="album">
-                  <img className="album__cover" crossOrigin="" src={track.artwork_url} alt={`album artwork from track ${track.title}`} onLoad={this.onLoadImage} />
+                  <img className="album__cover" src={track.artwork_url} alt={`album artwork from track ${track.title}`}/>
                 </div>
                 <div className="info">
                   <h2 className="info__track">{track.title}</h2>

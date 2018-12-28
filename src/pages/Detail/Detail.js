92c6d53cc7c0332ff03c841fde6d0d79de40ecf7
Ion D. Filho <iondrimba@gmail.com>
2018-12-28 17:07:53 -0200

    extract helper methods

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,26 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import ProgressBar from '../../components/ProgressBar/ProgressBar';
 import MediaButton from '../../components/MediaButton/MediaButton';
+import convertSecondsToMMss from '../../helpers/timer';
 import { ReactComponent as PlayButton } from '../../icons/play-arrow.svg';
 import { ReactComponent as PauseButton } from '../../icons/pause.svg';
 import styles from './styles.scss';
 
-const percent = (current, total) => {
-  return (current / total) * 100
-}
-
-const convertSecondsToMMss = (totalSeconds) => {
-  const sec_num = parseInt(totalSeconds, 10);
-  let hours = Math.floor(sec_num / 3600);
-  let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-  let seconds = sec_num - (hours * 3600) - (minutes * 60);
-
-  if (hours < 10) { hours = "0" + hours; }
-  if (minutes < 10) { minutes = "0" + minutes; }
-  if (seconds < 10) { seconds = "0" + seconds; }
-
-  return `${minutes}:${seconds}`;
-}
 
 class Detail extends Component {
   constructor(props) {
@@ -50,8 +35,8 @@ class Detail extends Component {
               <time className="time">{convertSecondsToMMss(this.props.track.duration / 1000)}</time>
             </div>
             <div className="detail__buttons">
-              <MediaButton onClick={this.onPlayClick} id={this.props.track.id} icon={<PlayButton width={26}/>} />
-              <MediaButton onClick={this.onPauseClick} id={this.props.track.id} icon={<PauseButton width={26}/>} />
+              <MediaButton active={this.props.track.playing && !this.props.track.paused} onClick={this.onPauseClick} icon={<PauseButton width={22}/>} />
+              <MediaButton active={!this.props.track.playing && this.props.track.paused} onClick={this.onPlayClick} icon={<PlayButton width={22}/>} />
             </div>
           </div>
         </div>

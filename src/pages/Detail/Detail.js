4021693793500a751f5103593de14055b233224b
Ion D. Filho <iondrimba@gmail.com>
2018-12-28 21:33:43 -0200

    add dynamic album cover shadow

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import FastAverageColor from 'fast-average-color/dist/index.es6';
 import ProgressBar from '../../components/ProgressBar/ProgressBar';
 import MediaButton from '../../components/MediaButton/MediaButton';
 import convertSecondsToMMss from '../../helpers/timer';
@@ -11,27 +12,34 @@ class Detail extends Component {
   constructor(props) {
     super(props);
 
-    this.onPauseClick = this.onPauseClick.bind(this);
-    this.onPlayClick = this.onPlayClick.bind(this);
+    this.fac = new FastAverageColor();
   }
 
-  onPlayClick() {
+   onPlayClick = () => {
     this.props.onPlayClick(this.props.track);
   }
 
-  onPauseClick() {
+  onPauseClick = () => {
     this.props.onPauseClick(this.props.track);
+  }
+
+  onLoadImage = async (evt) => {
+    const color = this.fac.getColor(evt.target, { algorithm: 'simple' });
+
+    const rgb =  color.hex.replace('#','').match(/[A-Za-z0-9]{2}/g).map(v => parseInt(v, 16));
+
+    evt.target.style.boxShadow = `0 24px 35px -16px rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, 0.7)`;
   }
 
   render() {
     return (
       <Fragment>
         <div className="detail__track">
-          <img className="detail__cover" crossOrigin="" src={this.props.track.artwork_url.replace('t67x67', 't300x300')} alt={`album artwork from track ${this.props.track.title}`} />
+          <img className="detail__cover" crossOrigin="" onLoad={this.onLoadImage} src={this.props.track.artwork_url.replace('t67x67', 't300x300')} alt={`album artwork from track ${this.props.track.title}`} />
           <div className="detail__controls">
             <div className="detail__info">
               <h3 className="title">{this.props.track.title}</h3>
-              <p className="artist">{this.props.track.user.username}</p>
+              <p className="artist">{this.props.track.artist}</p>
             </div>
             <ProgressBar percent={this.props.track.percentage} />
             <div className="detail__timing">

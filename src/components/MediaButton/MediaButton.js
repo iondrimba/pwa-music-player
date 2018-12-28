75e70902e80909dbfe5e7f547464f4225dd4868b
Ion D. Filho <iondrimba@gmail.com>
2018-12-28 17:07:29 -0200

    add active state to buttons

--- src/components/MediaButton/MediaButton.js
+++ src/components/MediaButton/MediaButton.js
@@ -2,5 +2,5 @@ import React from 'react';
 import styles from './styles.scss';
 
 export default (props) => {
-  return <button className="media-button" onClick={props.onClick} track-id={props.id}>{props.icon}</button>
+  return <button className={`media-button ${props.active ? 'media-button--active' : ''}`} onClick={props.onClick}>{props.icon}</button>
 };

bef3848da3da4a4237f26706a3a25450332951c4
Ion D. Filho <iondrimba@gmail.com>
2018-12-28 18:28:12 -0200

    update details view style

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -29,14 +29,20 @@ class Detail extends Component {
         <div className="detail__track">
           <img className="detail__cover" crossOrigin="" src={this.props.track.artwork_url.replace('t67x67', 't300x300')} alt={`album artwork from track ${this.props.track.title}`} />
           <div className="detail__controls">
+            <div className="detail__info">
+              <h3 className="title">{this.props.track.title}</h3>
+              <p className="artist">{this.props.track.user.username}</p>
+            </div>
             <ProgressBar percent={this.props.track.percentage} />
             <div className="detail__timing">
               <time className="time">{convertSecondsToMMss(this.props.track.currentTime)}</time>
               <time className="time">{convertSecondsToMMss(this.props.track.duration / 1000)}</time>
             </div>
             <div className="detail__buttons">
-              <MediaButton active={this.props.track.playing && !this.props.track.paused} onClick={this.onPauseClick} icon={<PauseButton width={22}/>} />
-              <MediaButton active={!this.props.track.playing && this.props.track.paused} onClick={this.onPlayClick} icon={<PlayButton width={22}/>} />
+              <button className="prev-button"><PlayButton width={16}/></button>
+              <MediaButton active={this.props.track.playing && !this.props.track.paused} onClick={this.onPauseClick} icon={<PauseButton width={28}/>} />
+              <MediaButton active={!this.props.track.playing && this.props.track.paused} onClick={this.onPlayClick} icon={<PlayButton width={28}/>} />
+              <button className="next-button"><PlayButton width={16}/></button>
             </div>
           </div>
         </div>

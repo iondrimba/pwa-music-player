4d38acc31971bb9458f09f5d0719a9dff441e701
Ion D. Filho <iondrimba@gmail.com>
2018-12-28 17:34:09 -0200

    improve placeholder data generation

--- src/app/data.js
+++ src/app/data.js
@@ -1,40 +1,14 @@
-export default [{
-  id: -1,
-  title: 'loading...',
-  user: {
-    username: 'loading...',
-  },
-  artwork_url: 'https://picsum.photos/100/100/?blur',
-},
-{
-  id: 2,
-  title: 'loading...',
-  user: {
-    username: 'loading...',
-  },
-  artwork_url: 'https://picsum.photos/100/100/?blur',
-},
-{
-  id: 3,
-  title: 'loading...',
-  user: {
-    username: 'loading...',
-  },
-  artwork_url: 'https://picsum.photos/100/100/?blur',
-},
-{
-  id: 4,
-  title: 'loading...',
-  user: {
-    username: 'loading...',
-  },
-  artwork_url: 'https://picsum.photos/100/100/?blur',
-},
-{
-  id: 5,
-  title: 'loading...',
-  user: {
-    username: 'loading...',
-  },
-  artwork_url: 'https://picsum.photos/100/100/?blur',
-}];
+const placeholderData = ()=> {
+  return [...new Array(10)].map((index, i) => {
+    return {
+      id: i,
+      title: 'loading...',
+      user: {
+        username: 'loading...',
+      },
+      artwork_url: 'https://picsum.photos/67/67/?blur',
+    }
+  });
+}
+
+export default [...placeholderData()];

973e68aa4cb6ab2f9c0c68cbf36c842776a15b4e
Ion D. Filho <iondrimba@gmail.com>
2019-01-01 15:20:53 -0200

    refactor list to be accessible via keyboard

--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -11,7 +11,7 @@ class List extends PureComponent {
   }
 
   onClick(evt) {
-    const id = Number(evt.currentTarget.attributes['item-id'].value);
+    const id = Number(evt.currentTarget.attributes['data-id'].value);
 
     this.props.onClick(id);
   }
@@ -19,12 +19,13 @@ class List extends PureComponent {
   render() {
     return (
       <Fragment>
-        <ul className="track-list">
+        <ul className="track-list" role="list">
           {
             this.props.tracks.map((track, i) => {
-              return <li className={`row ${this.props.track.id === track.id && this.props.track.playing ? 'playing' : ''}`} key={track.id} item-id={track.id} onClick={this.onClick}>
+              return <li role="list-item" className="row" key={track.id} >
+               <button className={`${this.props.track.id === track.id && this.props.track.playing ? 'playing' : ''}`} tabIndex={this.props.active ? "0" : "-1"} onClick={this.onClick} data-id={track.id}>
                 <div className="album">
-                  <img className="album__cover" src={track.artwork_url} alt={`album artwork from track ${track.title}`} />
+                  <img className="album__cover" src={track.artwork_url} alt={`Album artwork from track ${track.title}.`} />
                 </div>
                 <div className="info">
                   <h2 className="info__track">{track.title}</h2>
@@ -33,6 +34,7 @@ class List extends PureComponent {
                     <ProgressBar percent={this.props.track.id === track.id ? this.props.track.percentage : 0} />
                   </div>
                 </div>
+              </button>
               </li>
             })
           }

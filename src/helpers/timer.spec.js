e81773e3ab5e1ee6408dfec7d3f5c3a051d6f4fc
Ion D. Filho <iondrimba@gmail.com>
2019-01-26 21:19:29 -0200

    add timer helper spec

---
+++ src/helpers/timer.spec.js
@@ -0,0 +1,11 @@
+import timer from './timer';
+
+describe('timer', () => {
+  it('returns formatted time min:sec as string', () => {
+    const seconds = 360;
+
+    const result = timer(seconds);
+
+    expect(result).toBe('06:00');
+  });
+});

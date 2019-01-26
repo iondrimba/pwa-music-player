9befde215205901f98d111c06c0019004d46554b
Ion D. Filho <iondrimba@gmail.com>
2019-01-26 20:59:02 -0200

    add page initial specs

---
+++ src/components/Page/index.spec.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Page from './Page';
+import { enzymeConfig } from '../../enzimeConfig';
+
+enzymeConfig();
+
+describe('Page', () => {
+  it('renders defaultProps', () => {
+    const component = renderer.create(<Page
+      active={true}
+      className='home'
+      children={<div>Hello</div>}
+    />);
+
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});

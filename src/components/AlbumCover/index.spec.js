c4584b8416cb15fe98a6adc49c603d131de81c8b
Ion D. Filho <iondrimba@gmail.com>
2019-01-27 16:41:17 -0200

    add album cover specs

--- src/components/AlbumCover/index.spec.js
+++ src/components/AlbumCover/index.spec.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import AlbumCover from './AlbumCover';
-import { enzymeConfig } from '../../enzimeConfig';
+import sleep from '../../helpers/sleep';
+import { enzymeConfig, mount } from '../../enzimeConfig';
 
 enzymeConfig();
 
@@ -12,5 +13,46 @@ describe('AlbumCover', () => {
     const tree = component.toJSON();
 
     expect(tree).toMatchSnapshot();
+  });
+
+  describe('componentDidMount', () => {
+    it('calls onLoadImage', () => {
+      const component = mount(<AlbumCover src="https://i1.sndcdn.com/artworks-000021994411-0lr2l2-t300x300.jpg" alt="John Doe alt." />);
+
+      expect(component.instance().imageDummy.onload).toBeDefined();
+    });
+  });
+
+  describe('onLoadImage', () => {
+    it('calls _hideLoader _setAlbumShadowColor _displayAlbumCover', async () => {
+      const component = mount(<AlbumCover src="https://i1.sndcdn.com/artworks-000021994411-0lr2l2-t300x300.jpg" alt="John Doe alt." />);
+      const instance = component.instance();
+
+      jest.spyOn(AlbumCover.prototype, '_hideLoader');
+      jest.spyOn(AlbumCover.prototype, '_setAlbumShadowColor');
+      jest.spyOn(AlbumCover.prototype, '_displayAlbumCover');
+
+      await instance.onLoadImage({ target: instance.image.current });
+
+      await sleep(200);
+
+      expect(AlbumCover.prototype._hideLoader).toBeCalled();
+      expect(AlbumCover.prototype._setAlbumShadowColor).toBeCalled();
+      expect(AlbumCover.prototype._displayAlbumCover).toBeCalled();
+    });
+  });
+
+  describe('componentDidUpdate', () => {
+    it('calls _displayLoadingCover', async () => {
+      AlbumCover.prototype._displayLoadingCover = jest.fn();
+
+      const component = mount(<AlbumCover src="/john-doe.jpg" alt="John Doe alt." />);
+
+      component.setProps({ src: 'new-john-doe.jpg' });
+
+      await sleep(100);
+
+      expect(AlbumCover.prototype._displayLoadingCover).toBeCalled();
+    });
   });
 });

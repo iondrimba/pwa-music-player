28fcf4ad490ce91ba4ae8025e9e34d0aec76ea5e
Ion D. Filho <iondrimba@gmail.com>
2019-01-27 19:16:58 -0200

    refactor

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,6 +2,50 @@ import React, { PureComponent, Fragment } from 'react';
 import './styles.scss';
 
 class About extends PureComponent {
+  constructor() {
+    super();
+
+    this.state = {
+      credits: [
+        {
+          label: 'Design inspired by',
+          link: 'https://dribbble.com/shots/3000629-Music-Player-Sketch-Freebie',
+          author: 'Armas Nurbahari'
+        },
+        {
+          label: 'Songs API by',
+          link: 'https://developers.soundcloud.com/docs/api',
+          author: 'Soundcloud'
+        },
+        {
+          label: 'Pause icon by',
+          link: 'https://www.flaticon.com/authors/smashicons',
+          author: 'Smashicons'
+        },
+        {
+          label: 'Left arrow icon by',
+          link: 'https://www.flaticon.com/authors/lucy-g',
+          author: 'Lucy G'
+        },
+        {
+          label: 'Play arrow icon by',
+          link: 'https://www.freepik.com/',
+          author: 'Freepik'
+        },
+        {
+          label: 'Help icon by',
+          link: 'https://www.flaticon.com/authors/anton-saputro',
+          author: 'Anton Saputro'
+        },
+        {
+          label: 'Close icon by',
+          link: 'https://www.flaticon.com/authors/chanut',
+          author: 'Chanut'
+        },
+      ]
+    }
+  }
+
   render() {
     return (
       <Fragment>
@@ -10,20 +54,11 @@ class About extends PureComponent {
           <p>This is a personal project built in my spare time for learning purposes.</p>
           <h2>Credits:</h2>
           <ul>
-            <li>Design inspired by <a tabIndex="-1" href="https://dribbble.com/shots/3000629-Music-Player-Sketch-Freebie" target="_blank"  rel="noopener noreferrer"> Armas Nurbahari </a> </li>
-            <li>Songs API by <a tabIndex="-1" href="https://developers.soundcloud.com/docs/api" target="_blank"  rel="noopener noreferrer">Soundcloud</a></li>
-            <li>Bootstrapped with Create React App</li>
-            <li>Pause icon by <a tabIndex="-1" href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a tabIndex="-1" href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a tabIndex="-1" href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank"  rel="noopener noreferrer">CC 3.0 BY</a></li>
-            <li>Left arrow icon by <a tabIndex="-1" href="https://www.flaticon.com/authors/lucy-g" title="Lucy G">Lucy G</a> from <a tabIndex="-1" href="https://www.flaticon.com/"
-              title="Flaticon">www.flaticon.com</a> is licensed by <a tabIndex="-1" href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank"  rel="noopener noreferrer">CC 3.0 BY</a></li>
-            <li>Play arrow icon by <a tabIndex="-1" href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a tabIndex="-1" href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a tabIndex="-1" href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank"  rel="noopener noreferrer">CC 3.0 BY</a></li>
-
-            <li>Help icon by <a tabIndex="-1" href="https://www.flaticon.com/authors/anton-saputro" title="Anton Saputro">Anton Saputro</a> from <a tabIndex="-1" href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a tabIndex="-1" href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank"  rel="noopener noreferrer">CC 3.0 BY</a>
-            </li>
-            <li>
-              Close icon by <a tabIndex="-1" href="https://www.flaticon.com/authors/chanut" title="Chanut">Chanut</a> from <a tabIndex="-1" href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a tabIndex="-1" href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank"  rel="noopener noreferrer">CC 3.0 BY</a>
-            </li>
-
+            {
+              this.state.credits.map((license) => {
+                return <li>{license.label} <a tabIndex="-1" target="_blank" rel="noopener noreferrer" href={license.link} >{license.author}</a></li>
+              })
+            }
           </ul>
         </div>
 

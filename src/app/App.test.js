858852699ca187a21af68fc14fc33fdc3a33d828
Ion D. Filho <iondrimba@gmail.com>
2019-01-26 22:51:37 -0200

    add audio module helper

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AudioHelper from '../helpers/audio';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+jest.mock('../helpers/audio');
+jest.mock('../helpers/classlist');
+
+describe('App', () => {
+  beforeEach(() => {
+    AudioHelper.mockClear();
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+})

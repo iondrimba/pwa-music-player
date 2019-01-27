35dbddbc99021ca26ab0afa885b67638e68da1de
Ion D. Filho <iondrimba@gmail.com>
2019-01-27 20:44:40 -0200

    improve mediabutton spec

--- src/components/MediaButton/index.spec.js
+++ src/components/MediaButton/index.spec.js
@@ -2,20 +2,40 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import MediaButton from './MediaButton';
 import { ReactComponent as RepeatButton } from '../../icons/repeat-arrows.svg';
-import { enzymeConfig } from '../../enzimeConfig';
+import { enzymeConfig, shallow } from '../../enzimeConfig';
 
 enzymeConfig();
 
+const buildComponent = (active = true) => <MediaButton name='click me' active={active} icon={<RepeatButton className="icon icon--back" width={16} />} onClick={() => { }} />
+
 describe('MediaButton', () => {
   it('renders defaultProps', () => {
-    const component = renderer.create(<MediaButton
-      name='click me'
-      icon={<RepeatButton className="icon icon--back" width={16} />}
-      onClick={() => { }}
-    />);
+    const component = renderer.create(buildComponent());
 
     const tree = component.toJSON();
 
     expect(tree).toMatchSnapshot();
+  });
+
+  describe('shouldComponentUpdate', () => {
+    describe('when active prop has changed', () => {
+      it('return true', () => {
+        const component = shallow(buildComponent(true));
+
+        const result = component.instance().shouldComponentUpdate({ active: false });
+
+        expect(result).toBe(true);
+      });
+    });
+
+    describe('when active prop has not changed', () => {
+      it('return false', () => {
+        const component = shallow(buildComponent(true));
+
+        const result = component.instance().shouldComponentUpdate({ active: true });
+
+        expect(result).toBe(false);
+      });
+    });
   });
 });

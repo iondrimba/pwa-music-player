01994391f2187bf70d36210becb6ade23301f3de
Ion D. Filho <iondrimba@gmail.com>
2019-01-27 21:47:02 -0200

    move enzyme config to setup file

--- src/enzimeConfig.js
+++
@@ -1,6 +0,0 @@
-import Adapter from 'enzyme-adapter-react-16';
-import Enzyme, { shallow, mount } from 'enzyme';
-
-const enzymeConfig = () => { Enzyme.configure({ adapter: new Adapter() }); };
-
-export { shallow, mount, enzymeConfig };

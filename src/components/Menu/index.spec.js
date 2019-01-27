2755d8003227c28446252549e82961c9a98504e5
Ion D. Filho <iondrimba@gmail.com>
2019-01-27 15:48:02 -0200

    add componentDidMount componentDidUpdate specs

--- src/components/Menu/index.spec.js
+++ src/components/Menu/index.spec.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Menu from './Menu';
-import { enzymeConfig } from '../../enzimeConfig';
+import sleep from '../../helpers/sleep';
+import { enzymeConfig, mount } from '../../enzimeConfig';
 
 enzymeConfig();
 
@@ -18,4 +19,31 @@ describe('Menu', () => {
 
     expect(tree).toMatchSnapshot();
   });
+
+  describe('componentDidMount', () => {
+    it('adds class active to hidden elements', async () => {
+      Menu.prototype._activeHiddenElements = jest.fn();
+
+      const component = mount(<Menu activeView='home' onBackClick={() => { }} onAboutClick={() => { }}onCloseClick={() => { }} />);
+
+      await sleep(1300);
+
+      expect(component.instance()._activeHiddenElements).toBeCalled();
+    });
+  });
+
+  describe('componentDidUpdate', () => {
+    it('removes and adds class active to title', async () => {
+      jest.spyOn(Menu.prototype, '_animateTitle');
+
+      const component = mount(<Menu activeView='home' onBackClick={() => { }} onAboutClick={() => { }}onCloseClick={() => { }} />);
+
+      component.setProps({ activeView: 'about' });
+
+      await sleep(100);
+
+      expect(component.instance()._animateTitle).toBeCalled();
+    });
+  });
+
 });
